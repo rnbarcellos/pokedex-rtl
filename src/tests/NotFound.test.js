96fa2f6d9fe0b89f7d2b8d240d80d64f08dfa4bf
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-03-28 17:32:19 -0300

    test: case suites for NotFound page

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+// Components
+import NotFound from '../pages/NotFound';
+
+describe('Casos de teste da página NotFound', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(notFoundImg.src).toBe(
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
